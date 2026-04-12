a17a8af6a79d1908a5254223f36cb358e8206765
Urs Dreher <ursdreher@hotmail.com>
2026-04-12 20:36:01 +0200

    Appointment Model ergänzt

--- src/app/models/appointment.ts
+++ src/app/models/appointment.ts
@@ -1,10 +1,8 @@
-import { Customer } from './customer';
 import { Therapy } from './therapy';
 
 export interface Appointment {
   id: number;
   customerId: number;
-  customer: Customer;
   therapyId: number;
   therapy: Therapy;
   appointmentTimestamp: string;
